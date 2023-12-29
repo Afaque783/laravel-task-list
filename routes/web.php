73f1866ec781aa91fa4ff934971c0a721f2a9513
Afaque783 <ahmed.afaque783@gmail.com>
2023-12-29 18:26:41 +0530

    About Routing

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+// give name to Route by using name('Route Name')
+
+
+// difference between return redirect('/hello'); and return redirect()->route('hello'); 
+// is that if we change /hello route to  /xxx then return redirect()->route('hello');  will work properly 
+// Fallback Route is behave like a Route when user want to access the wrong route
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/app',function(){
-   return 'afaque';
+
+
+Route::get('/hello',function(){
+   return 'Hello';
+})->name('hello');
+
+
+Route::get('/hallo',function() {
+    return redirect('/hello');
 });
+
+
+Route::get('/hallo',function() {
+    return redirect()->route('hello');
+});
+
+
+
+Route::get('/greet/{name}',function($name) {
+    return 'Hello ' . $name . '!';
+});
+
+
+Route::fallback(function () {
+    return 'Still got Somewhere!';
+});
+
